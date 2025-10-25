0ad4e5c67fa69ac0ebf7a65fe23cff23fe70eb52
Kharka25 <adewoyindaniel6@gmail.com>
2025-10-25 09:33:55 +0100

    create views and note component

---
+++ app/src/main/java/com/example/stickynotes/screens/NoteList.kt
@@ -0,0 +1,24 @@
+package com.example.stickynotes.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.stickynotes.components.NoteCard
+import com.example.stickynotes.db.Note
+
+@Composable
+fun NoteList(notes: List<Note>) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)) {
+        items(notes){
+            note -> NoteCard(note)
+        }
+    }
+}
